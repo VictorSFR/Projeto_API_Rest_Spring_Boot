f55a406b651d65cc14b150f807b4cf11e6f20107
VictorSFR <victorsfranca3@outlook.com>
2022-10-10 17:34:46 -0300

    Funcionalidade de Autenticação com Spring security adicionada.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$kbtYlq1KK0H81S687DNrWuVAdNRyL7bQuQAJN8HyPPYXCaQp0V9Di');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
